b7148597ab4238921df87ac3b5ca6e5d594716d2
kkarbowiak <kkarbowiak@localhost>
2018-01-10 22:14:05 +0100

    Add capability to check existence in containers

--- isin.h
+++ isin.h
@@ -8,21 +8,22 @@
 #include <initializer_list>
 
 
-#define is_in(...) == isin_internal::is_in_get({__VA_ARGS__})
-#define is_not_in(...) != isin_internal::is_in_get({__VA_ARGS__})
+#define is_in(...) == isin_internal::is_in_get(__VA_ARGS__)
+#define is_not_in(...) != isin_internal::is_in_get(__VA_ARGS__)
 
 namespace isin_internal
 {
-    template<typename T>
+    template<class C>
     class is_in_check
     {
         public:
-            is_in_check(std::initializer_list<T> const & args)
+            is_in_check(C const & args)
                 : m_args(args)
             {
             }
 
-            friend bool operator==(T const & lhs, is_in_check<T> const & rhs)
+            template<typename T>
+            friend bool operator==(T const & lhs, is_in_check<C> const & rhs)
             {
                 for (auto a : rhs.m_args)
                 {
@@ -35,7 +36,8 @@ namespace isin_internal
                 return false;
             }
 
-            friend bool operator!=(T const & lhs, is_in_check<T> const & rhs)
+            template<typename T>
+            friend bool operator!=(T const & lhs, is_in_check<C> const & rhs)
             {
                 for (auto a : rhs.m_args)
                 {
@@ -49,13 +51,19 @@ namespace isin_internal
             }
 
         private:
-            std::initializer_list<T> const & m_args;
+            C const & m_args;
     };
 
-    template<typename T>
-    inline is_in_check<T> is_in_get(std::initializer_list<T> const & args)
+    template<class C>
+    inline is_in_check<C> is_in_get(C const & args)
     {
-        return is_in_check<T>(args);
+        return is_in_check<C>(args);
+    }
+
+    template<typename T>
+    inline is_in_check<std::initializer_list<T>> is_in_get(std::initializer_list<T> const & args)
+    {
+        return is_in_check<std::initializer_list<T>>(args);
     }
 }
 
