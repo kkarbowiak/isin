268b2437a06d1bb4418d06b2f6df3df412fe510d
kkarbowiak <kkarbowiak@localhost>
2018-01-11 22:21:55 +0100

    Add unit tests

--- test/unittest/test_isin.cpp
+++ test/unittest/test_isin.cpp
@@ -3,6 +3,9 @@
 
 #include "isin.h"
 
+#include <string>
+#include <vector>
+
 
 enum Values
 {
@@ -44,4 +47,31 @@ TEST_CASE("test is_in with char and braced-list")
     CHECK(v is_in({'a', 'b', 'c'}));
     CHECK_FALSE(v is_in({'a'}));
     CHECK_FALSE(v is_in({'a', 'c'}));
+}
+
+TEST_CASE("test is_in with std::string and braced-list")
+{
+    std::string v = "bar";
+
+    CHECK(v is_in({"bar"}));
+    CHECK(v is_in({"foo", "bar"}));
+    CHECK(v is_in({"foo", "bar", "baz"}));
+    CHECK_FALSE(v is_in({"foo"}));
+    CHECK_FALSE(v is_in({"foo", "baz"}));
+}
+
+TEST_CASE("test is_in with int and std::vector")
+{
+    int v = 2;
+    std::vector<int> vec1 = {2};
+    std::vector<int> vec2 = {1, 2};
+    std::vector<int> vec3 = {1, 2, 3};
+    std::vector<int> vec4 = {1};
+    std::vector<int> vec5 = {1, 3};
+
+    CHECK(v is_in(vec1));
+    CHECK(v is_in(vec2));
+    CHECK(v is_in(vec3));
+    CHECK_FALSE(v is_in(vec4));
+    CHECK_FALSE(v is_in(vec5));
 }
